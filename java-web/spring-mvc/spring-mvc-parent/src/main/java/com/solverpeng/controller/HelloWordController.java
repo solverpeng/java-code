57558506091dd8b584e2c30491e1d7da7a6739bc
solverpeng <javacoder_ssh@163.com>
2019-05-09 19:35:24 +0800

    no message

--- java-web/spring-mvc/spring-mvc-parent/src/main/java/com/solverpeng/controller/HelloWordController.java
+++ java-web/spring-mvc/spring-mvc-parent/src/main/java/com/solverpeng/controller/HelloWordController.java
@@ -3,22 +3,12 @@ package com.solverpeng.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class HelloWordController {
 
     @RequestMapping("/hello")
     public void helloWorld() {
         System.out.println("hello world!");
-    }
-
-    @RequestMapping("/encoding")
-    public void encoding(HttpServletRequest request, HttpServletResponse response) {
-        String characterEncoding = request.getCharacterEncoding();
-        String name = request.getParameter("name");
-        System.out.println("name=" + name + ", characterEncoding=" + characterEncoding);
     }
 
 }
