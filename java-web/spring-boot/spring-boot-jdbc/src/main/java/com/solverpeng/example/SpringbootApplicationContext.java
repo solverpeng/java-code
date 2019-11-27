eb15a0e70b8e40c2a2bbdd27e987e267a4ba3860
solverpeng <javacoder_ssh@163.com>
2019-11-27 14:38:38 +0800

    123

---
+++ java-web/spring-boot/spring-boot-jdbc/src/main/java/com/solverpeng/example/SpringbootApplicationContext.java
@@ -0,0 +1,11 @@
+package com.solverpeng.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationContext {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplicationContext.class, args);
+    }
+}
---
+++ java-web/spring-boot/spring-boot-jdbc/src/test/java/com/solverpeng/example/SpringbootApplicationContextTest.java
@@ -0,0 +1,26 @@
+package com.solverpeng.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationContextTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void dataSourceTest() throws SQLException {
+        System.out.println(dataSource.getClass());
+        System.out.println(dataSource.getConnection());
+    }
+
+}
